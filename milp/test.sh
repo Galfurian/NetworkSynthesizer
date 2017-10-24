061483f67a0558952cfdbd858ed551a962ab5c09
Galfurian <enry.frak@gmail.com>
2017-10-24 17:52:50 +0200

    Add script for automated testing

---
+++ milp/test.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+rm -f result.txt
+python Synthesizer.py examples/TestCase1/input.txt examples/nodes.txt examples/channels.txt 1
+python Synthesizer.py examples/TestCase1/input.txt examples/nodes.txt examples/channels.txt 2
+python Synthesizer.py examples/TestCase1/input.txt examples/nodes.txt examples/channels.txt 3
+python Synthesizer.py examples/TestCase1/input.txt examples/nodes.txt examples/channels.txt 4
+
+python Synthesizer.py examples/TestCase2/input.txt examples/nodes.txt examples/channels.txt 1
+python Synthesizer.py examples/TestCase2/input.txt examples/nodes.txt examples/channels.txt 2
+python Synthesizer.py examples/TestCase2/input.txt examples/nodes.txt examples/channels.txt 3
+python Synthesizer.py examples/TestCase2/input.txt examples/nodes.txt examples/channels.txt 4
+
+python Synthesizer.py examples/TestCase3/input.txt examples/nodes.txt examples/channels.txt 1
+python Synthesizer.py examples/TestCase3/input.txt examples/nodes.txt examples/channels.txt 2
+python Synthesizer.py examples/TestCase3/input.txt examples/nodes.txt examples/channels.txt 3
+python Synthesizer.py examples/TestCase3/input.txt examples/nodes.txt examples/channels.txt 4
+
+python Synthesizer.py examples/TestCase4/input.txt examples/nodes.txt examples/channels.txt 1
+python Synthesizer.py examples/TestCase4/input.txt examples/nodes.txt examples/channels.txt 2
+python Synthesizer.py examples/TestCase4/input.txt examples/nodes.txt examples/channels.txt 3
+python Synthesizer.py examples/TestCase4/input.txt examples/nodes.txt examples/channels.txt 4
+
+python Synthesizer.py examples/Office/input.txt examples/nodes.txt examples/channels.txt 1
+python Synthesizer.py examples/Office/input.txt examples/nodes.txt examples/channels.txt 2
+python Synthesizer.py examples/Office/input.txt examples/nodes.txt examples/channels.txt 3
+python Synthesizer.py examples/Office/input.txt examples/nodes.txt examples/channels.txt 4
+
+python Synthesizer.py examples/Office10/input.txt examples/nodes.txt examples/channels.txt 1
+python Synthesizer.py examples/Office10/input.txt examples/nodes.txt examples/channels.txt 2
+python Synthesizer.py examples/Office10/input.txt examples/nodes.txt examples/channels.txt 3
+python Synthesizer.py examples/Office10/input.txt examples/nodes.txt examples/channels.txt 4
