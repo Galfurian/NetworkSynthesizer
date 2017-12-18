f8d2817818f7c9a723eea84eee2ee916a592b48d
Enrico Fraccaroli <enrico.fraccaroli@univr.it>
2017-12-18 20:52:38 +0100

    Update stresstest script

--- milp/stress_test.sh
+++ milp/stress_test.sh
@@ -2,16 +2,16 @@
 rm -f *.txt
 rm -f *.log
 
-echo "START" > result.txt
+echo "START" >> result.txt
 
 xterm -geometry 280x31+100+350 -e tail -f result.txt &
 
 for directory in $(find stress_test -maxdepth 1 -mindepth 1 -type d | sort -V);
 do
-	python Synthesizer.py $directory/input.txt examples/nodes.txt examples/channels.txt 1
-	python Synthesizer.py $directory/input.txt examples/nodes.txt examples/channels.txt 2
-	python Synthesizer.py $directory/input.txt examples/nodes.txt examples/channels.txt 3
-	python Synthesizer.py $directory/input.txt examples/nodes.txt examples/channels.txt 4
+	python Synthesizer.py $directory/input.txt stress_test/nodes.txt stress_test/channels.txt 1
+	python Synthesizer.py $directory/input.txt stress_test/nodes.txt stress_test/channels.txt 2
+	python Synthesizer.py $directory/input.txt stress_test/nodes.txt stress_test/channels.txt 3
+	python Synthesizer.py $directory/input.txt stress_test/nodes.txt stress_test/channels.txt 4
 done
 
 
