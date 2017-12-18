9f160edf63cb69e2f40c2e74de29904b42143ea9
Galfurian <enry.frak@gmail.com>
2017-12-18 18:43:32 +0100

    Add stress test.

---
+++ milp/stress_test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+rm -f *.txt
+rm -f *.log
+
+echo "START" > result.txt
+
+xterm -geometry 280x31+100+350 -e tail -f result.txt &
+
+for directory in $(find stress_test -maxdepth 1 -mindepth 1 -type d | sort -V);
+do
+	python Synthesizer.py $directory/input.txt examples/nodes.txt examples/channels.txt 1
+	python Synthesizer.py $directory/input.txt examples/nodes.txt examples/channels.txt 2
+	python Synthesizer.py $directory/input.txt examples/nodes.txt examples/channels.txt 3
+	python Synthesizer.py $directory/input.txt examples/nodes.txt examples/channels.txt 4
+done
+
+
+echo "DONE" >> result.txt
